4caf287755003893cae471e8dd3cbdd5645bc9b0
NatáliaFonseca <fonsecanatalianf@gmail.com>
2023-08-29 11:23:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function celular (modelo, fabricante, anoFabricacao){
+this.modelo=modelo;
+this.fabricante=fabricante;
+this.anoFabricacao=anoFabricacao;
+}
+
+
+
+const celular1= new celular("iphone", "apple", 2019)
+const celular2= new celular ("galaxy", "samsung", 2018)
+const celular3= new celular ("redmi", "xiomi", 2020)
+
+console.log (celular1)
+console.log (celular2)
+console.log (celular3)
